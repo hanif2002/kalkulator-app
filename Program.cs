27ab51e4bc71ddd68a707c967d4062a6a92d909f
haniff <muhamadhaniff3390@students.amikom.ac.id>
2021-03-12 20:01:06 +0700

    installasi project

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Pemograman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Kalkulator";
+
+            int a = 0;
+            int b = 0;
+
+            int total = 0;
+
+            Console.Write("Masukan angka a : ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukan angka b : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("1. Penjumlahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Pembagian");
+            Console.WriteLine("4. Perkalian");
+            Console.Write("Masukan Operator : ");
+            Console.Write("Masukan Operator : ");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    total = a + b;
+                    Console.WriteLine("Hasil : " + total);
+                    break;
+                case "2":
+                    total = a - b;
+                    Console.WriteLine("Hasil : " + total);
+                    break;
+                case "3":
+                    total = a / b;
+                    Console.WriteLine("Hasil : " + total);
+                    break;
+                case "4":
+                    total = a * b;
+                    Console.WriteLine("Hasil : " + total);
+                    break;
+
+                    
+
+                   
+            }
+        }
+    }
+}
